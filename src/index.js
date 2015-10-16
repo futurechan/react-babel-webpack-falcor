455ffd844dd091779bedb8f00b6405ebc0c310fd
Josh <joshua.bennett.chan@gmail.com>
2015-10-16 00:02:05 -0500

    Atom Data Type

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const React = require('react');
 
+const $ref = falcor.Model.ref;
+const $atom = falcor.Model.atom;
+
 const model = new falcor.Model({
 	//source: new falcor.HttpDataSource('./model.json')
 	cache: {
@@ -19,15 +22,17 @@ const model = new falcor.Model({
 				instructions: "Bake them lol",
 				ingredients: [
 					{ $type: 'ref', value: ["ingredientsById",1] },
-					{ $type: 'ref', value: ["ingredientsById",2] }
-				]
+					$ref("ingredientsById[2]")
+				],
+				authors: $atom(["Brian", "John", "Other Person"])
 			},
 			{
 				name: "Brownies",
 				instructions: "Also bake them rofl",
 				ingredients: [
 					{ $type: 'ref', value: ["ingredientsById",1] }
-				]
+				],
+				authors: { $type: 'atom', value: ["Ringo", "Sam"]}
 			}
 		]
 	}
@@ -35,7 +40,7 @@ const model = new falcor.Model({
 
 //model.get(['recipes', 0, ['name', 'instructions']])
 //model.get('recipes[0..1]["name","instructions"]')
-model.get('recipes[0..1].ingredients[0..9]["name", "description"]', 'recipes[0..1]["name", "instructions"]')
+model.get('recipes[0..1].ingredients[0..9]["name", "description"]', 'recipes[0..1]["name", "instructions", "authors"]')
 	.then( data => {
 		console.log(data);
 	})
